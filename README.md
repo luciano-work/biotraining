7b1a1dffa656d1fe1e3a3a3ceb257126d06f88b7
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-08-25 09:32:47 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center">
+    <!-- <img src="src/assets/images/logo.png" width="80"> -->
+</p>
+
+### Screenshots
+![Screenshot - Prize Draw](src/assets/images/preview.png)
+
+#### Deploy on Github Pages
+`git subtree push --prefix src origin gh-pages`
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
+    mode: 'jit',
+    important: false,
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'system-ui', 'sans-serif']
+            },
+            colors: {
+                primary: colors.orange,
+                dark: colors.black,
+            },
+        },
+    },
+    variants: {
+        extend: {},
+        scrollbar: ['dark', 'rounded']
+    },
+    plugins: [
+        require('tailwind-scrollbar')
+    ],
+}
